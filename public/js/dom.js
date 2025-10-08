713547af5b2f11d8e7ccb52c6cc53ef9bfaa1810
Atef Moazzen <68507159+atef1995@users.noreply.github.com>
2025-10-08 21:35:31 +0200

    add window onresize to center the box

---
+++ public/js/dom.js
@@ -0,0 +1,10 @@
+const box = document.getElementById("box")
+const centerBox = () => {
+  console.log(window.innerHeight + " ," + window.innerWidth)
+  box.style.position = "absolute"
+  box.style.top = window.innerHeight / 2 + "px"
+  box.style.right = window.innerWidth / 2 + "px"
+}
+centerBox()
+
+window.onresize = centerBox
