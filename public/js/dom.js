99e80e6e08d6b14be7bf04a305f9b497bd8dd911
Atef Moazzen <68507159+atef1995@users.noreply.github.com>
2025-10-13 22:40:50 +0200

    feat: toggle between circular and square enhance: moved logic into toggleSelected fucntion for outline

--- public/js/dom.js
+++ public/js/dom.js
@@ -10,15 +10,17 @@ centerBox()
 
 window.onresize = centerBox
 
-box.addEventListener('click', () => {
+const toggleSelected = () => {
   if (box.classList.contains('outline')) {
-    box.classList.remove('outline')
+    box.classList.remove('outline', 'selected')
   } else {
-    box.classList.add('outline')
+    box.classList.add('outline', 'selected')
   }
-})
+}
 
-box.addEventListener('dblclick', () => {
+box.addEventListener('click', () => toggleSelected())
+
+const doubleClick = () => {
   box.classList.add('animateSize')
   box.style.width = "2px"
   box.style.height = "2px"
@@ -26,4 +28,25 @@ box.addEventListener('dblclick', () => {
   setTimeout(() => {
     box.remove()
   }, 2000)
+}
+
+box.addEventListener('dblclick', () => doubleClick())
+
+const keyboardPress = (ev) => {
+}
+
+document.addEventListener('keydown', (ev) => {
+  ev.preventDefault()
+
+  const selectedBoxes = document.getElementsByClassName('selected')
+  if (ev.key === "e") {
+    for (let index = 0; index < selectedBoxes.length; index++) {
+      const element = selectedBoxes[index]
+      if (element.classList.contains("circular")) {
+        element.classList.remove("circular")
+      } else {
+        element.classList.add("circular")
+      }
+    }
+  }
 })
