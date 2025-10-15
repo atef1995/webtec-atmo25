53dd55f55e0494f196c91d376708a867da100eca
Atef Moazzen <68507159+atef1995@users.noreply.github.com>
2025-10-15 12:44:06 +0200

    feat: enhance keyboard controls for box movement and add out-of-bounds handling

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,3 +1,5 @@
+const movementStep = 15
+
 const box = document.getElementById('box')
 const boxes = document.getElementsByClassName('box')
 const centerBox = () => {
@@ -31,20 +33,39 @@ const doubleClick = () => {
 
 box.addEventListener('dblclick', () => doubleClick())
 
+/**
+ * Handles out of bounds
+ * @param {HTMLElement} element 
+ */
+const onOutOfBounds = (element) => {
+  let rightPxNum = parseInt(element.style.right.replace('px', ''))
+  let topPxNum = parseInt(element.style.top.replace('px', ''))
+  const elementWidth = element.offsetWidth
+  const elementHeight = element.offsetHeight
+  const windowInnerWidth = parseInt(window.innerWidth)
+  const windowInnerHeight = parseInt(window.innerHeight)
+
+  if (rightPxNum > windowInnerWidth - elementWidth) {
+    rightPxNum = 0
+  } else if (rightPxNum < 0) {
+    rightPxNum = windowInnerWidth - elementWidth + 10
+  } else if (topPxNum > windowInnerHeight - elementHeight) {
+    topPxNum = 0
+  } else if (topPxNum < 0) {
+    topPxNum = windowInnerHeight - elementHeight
+  }
+  console.log({
+    windowInnerHeight, windowInnerWidth,
+    elementWidth, element, rightPxNum, topPxNum
+  })
+
+  return [rightPxNum, topPxNum]
+}
+
+/**
+ * @param {KeyboardEvent} ev 
+ */
 const keyboardPress = (ev) => {
-}
-
-const deleteElement = (element) => {
-  element.classList.add('animateSize')
-  element.style.width = '2px'
-  element.style.height = '2px'
-
-  setTimeout(() => {
-    element.remove()
-  }, 2000)
-}
-
-document.addEventListener('keydown', (ev) => {
   ev.preventDefault()
   const colorArray = ['bg-red', 'bg-orange', 'bg-yellow', 'bg-green', 'bg-blue', 'bg-indigo', 'bg-violet']
   const selectedBoxes = document.getElementsByClassName('selected')
@@ -98,5 +119,50 @@ document.addEventListener('keydown', (ev) => {
     newBox.addEventListener('click', (e) => toggleSelected(e.target))
 
     document.body.appendChild(newBox)
+  } else if (ev.key === 'ArrowUp') {
+    console.log("up pressed")
+
+    for (let index = 0; index < selectedBoxes.length; index++) {
+      const element = selectedBoxes[index]
+      const [_, topPxNum] = onOutOfBounds(element)
+      element.style.top = topPxNum - movementStep + 'px'
+      console.log(element.style.top)
+
+    }
+  } else if (ev.key === 'ArrowRight') {
+
+    for (let index = 0; index < selectedBoxes.length; index++) {
+      const element = selectedBoxes[index]
+      const [rightPxNum] = onOutOfBounds(element)
+      element.style.right = rightPxNum - movementStep + 'px'
+    }
   }
-})
+  else if (ev.key === 'ArrowLeft') {
+    for (let index = 0; index < selectedBoxes.length; index++) {
+      const element = selectedBoxes[index]
+      const [rightPxNum] = onOutOfBounds(element)
+
+      element.style.right = rightPxNum + movementStep + 'px'
+    }
+  } else if (ev.key === 'ArrowDown') {
+
+    for (let index = 0; index < selectedBoxes.length; index++) {
+      const element = selectedBoxes[index]
+      const [_, topPxNum] = onOutOfBounds(element)
+      element.style.top = topPxNum + movementStep + 'px'
+    }
+  }
+
+}
+
+const deleteElement = (element) => {
+  element.classList.add('animateSize')
+  element.style.width = '2px'
+  element.style.height = '2px'
+
+  setTimeout(() => {
+    element.remove()
+  }, 2000)
+}
+
+document.addEventListener('keydown', (ev) => keyboardPress(ev))
